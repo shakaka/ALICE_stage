01d64bfe4ff4c27f2e2cf2be5b15207ebde91ca2
shakaka <midknightyeh@hotmail.com>
2017-03-31 11:47:32 +0200

    changing charge distinguisher

--- testing/SimplePtTask/AliAnalysisTaskSimplePt.cxx
+++ testing/SimplePtTask/AliAnalysisTaskSimplePt.cxx
@@ -345,7 +345,7 @@ void AliAnalysisTaskSimplePt::UserExec(Option_t *)
       continue;
     }
     if ( ! fMuonTrackCuts->IsSelected(track1) ) continue;
-    ( (TH1F*)fOutput->UncheckedAt(kDiMuCh) )->Fill(track1->Charge());
+
 
     for (Int_t jTrack = iTrack+1; jTrack < nTracks; jTrack++) {
       AliAODTrack *track2 = (AliAODTrack*) fAODEvent->GetTrack(jTrack);
@@ -356,7 +356,8 @@ void AliAnalysisTaskSimplePt::UserExec(Option_t *)
       }
       if ( ! fMuonTrackCuts->IsSelected(track2) ) continue;
 
-      if(track1->Charge()*track2->Charge()==1) continue;
+
+      if(!(track1->Charge()*track2->Charge())==-1) continue;
 
       Float_t muonMass2 = AliAnalysisMuonUtility::MuonMass2();
       TLorentzVector lvMuon1, lvMuon2, lvDimuon;
@@ -369,7 +370,7 @@ void AliAnalysisTaskSimplePt::UserExec(Option_t *)
       Double_t plDiMu = TMath::Sqrt(lvDimuon.P()*lvDimuon.P()-lvDimuon.Pt()*lvDimuon.Pt());
 
 
-      //Int_t chDiMu = track1->Charge()*track2->Charge();
+      Int_t chDiMu = track1->Charge()*track2->Charge();
 
 
       Double_t zvMu1 = track1->Zv();
@@ -386,7 +387,7 @@ void AliAnalysisTaskSimplePt::UserExec(Option_t *)
       ( (TH1F*)fOutput->UncheckedAt(kDiMuY) )->Fill(yDiMu);
       ( (TH1F*)fOutput->UncheckedAt(kDiMuPhi) )->Fill(phiDiMu);
       ( (TH1F*)fOutput->UncheckedAt(kDiMuZv) )->Fill(disDiMu);
-
+      ( (TH1F*)fOutput->UncheckedAt(kDiMuCh) )->Fill(chDiMu);
 
 
 
